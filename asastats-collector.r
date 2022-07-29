62a491304ff4b2e5845e89f85b08bc244173b608
athekat <63990513+athekat@users.noreply.github.com>
2022-07-28 22:59:17 -0300

    Add files via upload

---
+++ asastats-collector.r
@@ -0,0 +1,25 @@
+library(dplyr)
+library(tidyr)
+shell.exec("get-info.bat")
+
+asahistory <- read.csv("asahistory.csv") #La primera vez no se ejecuta.
+asadf <- read.delim2("results.txt", header = FALSE, sep = " ", dec = ".")
+
+asadf <- subset(asadf, select = -c(1,2,3,6,7,8,9,10))
+colnames(asadf) <- c("algoprice","usd")
+asadf$algoprice<- gsub("data-pricealgo=","",as.character(asadf$algoprice))
+asadf$usd<- gsub("data-total=","",as.character(asadf$usd))
+asadf$algoprice = as.numeric(as.character(asadf$algoprice))
+asadf$usd = as.numeric(as.character(asadf$usd))
+asadf$algos <- c(asadf$usd/asadf$algoprice)
+asadf$date <-format(Sys.time(), "%D")
+asadf <- select(asadf, date, usd, algoprice, algos)
+
+# write.csv(asadf, "asahistory.csv", row.names=FALSE) Luego de la primera no volver a ejecutar
+
+asahistory <- rbind(asahistory, asadf) 
+
+write.csv(asahistory, "asahistory.csv", row.names=FALSE)
+
+file.remove('results.txt')
+file.remove('download.txt')
---
+++ get-info.bat
@@ -0,0 +1,4 @@
+cd desktop
+curl https://www.asastats.com/6A4686C21DF78B14C14C06F58555A2699BBEC37C > download.txt
+findstr "<h1" download.txt >> results.txt
+rscript asastats-collector.r
